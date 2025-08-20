f40bf3dbaf95b167176377407b32d80e44328df3
Tony Tomy <josephtony2002@gmail.com>
2025-08-20 10:58:46 +0100

    changes added to analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
+import { inject } from '@vercel/analytics';
 import { createRoot } from 'react-dom/client'
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import './index.css'
 import App from './App.tsx'
 
+
+inject();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    
   </StrictMode>,
 )
